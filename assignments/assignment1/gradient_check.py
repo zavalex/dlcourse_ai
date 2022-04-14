721da32ca1143e4b1b35e2b4fef9dcdcd5ab16b2
Alexey Zavalskiy <papyas12@hotmail.com>
2022-04-14 11:58:44 +0300

    First assingnment progress

--- assignments/assignment1/gradient_check.py
+++ assignments/assignment1/gradient_check.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def check_gradient(f, x, delta=1e-5, tol = 1e-4):
+def check_gradient(f, x, delta=1e-5, tol=1e-4):
     '''
     Checks the implementation of analytical gradient by comparing
     it to numerical gradient using two-point formula
@@ -15,35 +15,36 @@ def check_gradient(f, x, delta=1e-5, tol = 1e-4):
     Return:
       bool indicating whether gradients match or not
     '''
-    
+
     assert isinstance(x, np.ndarray)
     assert x.dtype == np.float
-    
+
     orig_x = x.copy()
     fx, analytic_grad = f(x)
-    assert np.all(np.isclose(orig_x, x, tol)), "Functions shouldn't modify input variables"
+    assert np.all(np.isclose(orig_x, x, tol)
+                  ), "Functions shouldn't modify input variables"
 
     assert analytic_grad.shape == x.shape
     analytic_grad = analytic_grad.copy()
 
     # We will go through every dimension of x and compute numeric
     # derivative for it
+    XDelta = np.zeros_like(x)
     it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
     while not it.finished:
         ix = it.multi_index
         analytic_grad_at_ix = analytic_grad[ix]
-        numeric_grad_at_ix = 0
+        XDelta = np.zeros_like(x)
+        XDelta[ix]=delta
+        numeric_grad_at_ix = (
+            (f(x[ix]+XDelta)[0] - f(x[ix]-XDelta)[0]) / (2 * delta))
 
-        # TODO compute value of numeric gradient of f to idx
         if not np.isclose(numeric_grad_at_ix, analytic_grad_at_ix, tol):
-            print("Gradients are different at %s. Analytic: %2.5f, Numeric: %2.5f" % (ix, analytic_grad_at_ix, numeric_grad_at_ix))
+            print("Gradients are different at %s. Analytic: %2.5f, Numeric: %2.5f" % (
+                ix, analytic_grad_at_ix, numeric_grad_at_ix))
             return False
 
         it.iternext()
 
     print("Gradient check passed!")
     return True
-
-        
-
-        
